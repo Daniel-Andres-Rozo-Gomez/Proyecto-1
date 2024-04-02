301e332644a39717b91e3fe7adb590e1a0faded6
Marlen-Lizeth-Estrada-Lopez <marlen.estrada@correounivalle.edu.co>
2024-04-02 16:38:30 -0500

    Realizacion del menu y la estructura switch

--- Proyecto 1/src/App.java
+++ Proyecto 1/src/App.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner seleccion= new Scanner(System.in);
+        System.out.println("Bienvenido a Bancolombia");
+        System.out.println("1. Crear cliente");
+        System.out.println("2. Insertar dinero ahorrado");
+        System.out.println("3. Actualizar dinero ahorrado");
+        System.out.println("4. Eliminar dinero ahorrado");
+        System.out.println("5. Buscar cliente por nombre y ver su ahorro");
+        System.out.println("6. Listar todos los clientes");
+        System.out.println("Seleccione la opción que desee: ");
+        int eleccion=seleccion.nextInt();
     }
 }
