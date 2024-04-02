86b5dbcff0eca85340c24b9bcb468646251e337c
Marlen-Lizeth-Estrada-Lopez <marlen.estrada@correounivalle.edu.co>
2024-04-02 16:46:52 -0500

    Ya se agrego la estructura swtich

--- Proyecto 1/src/App.java
+++ Proyecto 1/src/App.java
@@ -10,7 +10,31 @@ public class App {
         System.out.println("4. Eliminar dinero ahorrado");
         System.out.println("5. Buscar cliente por nombre y ver su ahorro");
         System.out.println("6. Listar todos los clientes");
+
         System.out.println("Seleccione la opción que desee: ");
         int eleccion=seleccion.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                break;
+        
+            case 2:
+                break;
+            
+            case 3:
+                break;
+            
+            case 4:
+                break;
+
+            case 5:
+                break;
+            
+            case 6:
+                break;
+            
+            default:
+                break;
+        }
     }
 }
