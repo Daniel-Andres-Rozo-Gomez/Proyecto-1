2922c8f6134a647e357b0e69ab3a1653a5437c32
Daniel-Andres-Rozo-Gomez <daniel.andres.rozo@correounivalle.edu.co>
2024-04-03 01:48:33 -0500

    Solicito revision nuevamente del avance

--- Proyecto 1/src/App.java
+++ Proyecto 1/src/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<cliente> listaDeclientes = new ArrayList<cliente>();
         System.out.println("Bienvenido a Bancolombia");
+        
     }
 }
